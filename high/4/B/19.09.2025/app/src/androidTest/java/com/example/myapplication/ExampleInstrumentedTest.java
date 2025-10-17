957536aa8304fc22416b7ac90163908d86fbda27
Wolfyxon <wolfyxon@gmail.com>
2025-10-17 11:01:49 +0200

    Reinitialized the project because gradle refuses to be a normal build tool

--- high/4/B/19.09.2025/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ high/4/B/19.09.2025/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.wolfyxon.myapplication;
+package com.example.myapplication;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.wolfyxon.myapplication", appContext.getPackageName());
+        assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 }
--- high/4/B/19.09.2025/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ high/4/B/19.09.2025/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.wolfyxon.myapplication;
+package com.example.myapplication;
 
 import org.junit.Test;
 
