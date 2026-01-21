1d99e73e87ce82faa8ad476666d15708e6880313
Wolfyxon <wolfyxon@gmail.com>
2026-01-21 10:00:43 +0100

    21.01.2026

---
+++ high/4/T/21.01.2026/app/src/main/java/com/example/seekcolor/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.seekcolor;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView colorLabel;
+    SeekBar seekR;
+    SeekBar seekG;
+    SeekBar seekB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        seekR = setupBar(findViewById(R.id.seekR));
+        seekG = setupBar(findViewById(R.id.seekG));
+        seekB = setupBar(findViewById(R.id.seekB));
+
+        update();
+    }
+
+    void update() {
+        colorLabel.setBackgroundColor(Color.rgb(seekR.getProgress(), seekG.getProgress(), seekB.getProgress()));
+    }
+
+    SeekBar setupBar(SeekBar bar) {
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                update();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+}
