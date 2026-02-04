22c66a83c028075e1aa8c36ebf7e7eccb8b7cafc
Wolfyxon <wolfyxon@gmail.com>
2026-02-04 10:09:34 +0100

    init 04.02.2026

---
+++ high/4/T/04.02.2026/app/src/main/java/com/example/seekthing/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.seekthing;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText input;
+    TextView label;
+    SeekBar sizeSlider;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        radioGroup = findViewById(R.id.radio_group);
+        input = findViewById(R.id.input);
+        label = findViewById(R.id.label);
+        sizeSlider = findViewById(R.id.size_slider);
+
+        addColor("Czarny", 0x000000);
+        addColor("Niebieski", 0x0000FF);
+        addColor("Czerwony", 0xFF0000);
+        addColor("Zielony", 0x00FF00);
+
+        findViewById(R.id.btn_apply).setOnClickListener(l -> {
+            label.setText(input.getText());
+        });
+
+        sizeSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                label.setTextSize(sizeSlider.getProgress());
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    void addColor(String name, int value) {
+        RadioButton radio = new RadioButton(this);
+
+        radio.setText(name);
+        radio.setTextColor(value);
+
+        radio.setOnClickListener(l -> {
+            label.setTextColor(value);
+        });
+
+        radioGroup.addView(radio);
+    }
+}
